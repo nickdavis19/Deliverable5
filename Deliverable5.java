2933aac06c9806f6f902cd8624ad5e9cc22c3079
nickdavis19 <nid34@pitt.edu>
2017-11-19 13:15:13 -0500

    refactored to be ints
    Refactored generateExhaustive to be an arrayList of int[] instead of Strings. I atomize them and do comparisons now to find the covering array

--- Deliverable5.java
+++ Deliverable5.java
@@ -4,7 +4,7 @@ import java.lang.StringBuilder;
 public class Deliverable5 {
 	
 	public static void main(String[] args){
-		ArrayList<String> exhaustive = new ArrayList<String>();
+		ArrayList<int[]> exhaustive = new ArrayList<int[]>();
 		int numberOfArgs = args.length;
 		for(int i=0; i<numberOfArgs; i++){ //prints the names of the args
 			System.out.print(args[i] + "\t");
@@ -12,7 +12,11 @@ public class Deliverable5 {
 		System.out.println();
 		exhaustive = generateExhaustive(numberOfArgs);
 		for(int i=0; i<exhaustive.size(); i++){
-			System.out.println(exhaustive.get(i));
+			int[] tempArray = exhaustive.get(i);
+			for(int j=0; j<tempArray.length; j++){
+				System.out.print(tempArray[j] +"\t");
+			}
+			System.out.println();
 		}
 	}
 	
@@ -20,17 +24,18 @@ public class Deliverable5 {
 	//generateExhaustive generates the exhaustive truth table for the number of arguements entered
 	//parameters: int num: number of arguements entered when running the program
 	//returns: ArrayList of strings where each element is the string representation of a line of the exhaustive truth table
-	public static ArrayList<String> generateExhaustive(int num){
-		ArrayList<String> temp = new ArrayList<String>();
+	public static ArrayList<int[]> generateExhaustive(int num){
+		ArrayList<int[]> temp = new ArrayList<int[]>();
 		int rows = (int) Math.pow(2,num); //number of rows in an exhuastive truth table is 2^num
 		for(int i=0; i<rows; i++){
-			StringBuilder tempBuilder = new StringBuilder();
+			int[] tempArray = new int[num];
+			int k = 0;
 			for(int j=num-1; j>=0; j--){
 				int tempInt = i/(int) Math.pow(2,j)%2;
-				tempBuilder.append(tempInt);
-				tempBuilder.append("\t");
+				tempArray[k] = tempInt;
+				k++;
 			}
-			temp.add(tempBuilder.toString());
+			temp.add(tempArray);
 		}
 		return temp;
 	}
